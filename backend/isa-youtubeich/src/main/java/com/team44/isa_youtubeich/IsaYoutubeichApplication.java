c7d4cf4974b93c244b40665c8c4e4919f092568d
Vladimir Zoric <vladimir.zorke@gmail.com>
2026-01-02 01:25:17 +0100

    feat(backend): add initial backend project

---
+++ backend/isa-youtubeich/src/main/java/com/team44/isa_youtubeich/IsaYoutubeichApplication.java
@@ -0,0 +1,13 @@
+package com.team44.isa_youtubeich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaYoutubeichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaYoutubeichApplication.class, args);
+	}
+
+}
