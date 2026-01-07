3d7047e8462e618f4b2e284b9e9d29a9be5d5a5b
Mile Vuletić <vuletic.ra45.2022@uns.ac.rs>
2026-01-07 22:14:08 +0100

    feat(frontend): make Header fixed on top

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -5,25 +5,28 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Header() {
   return (
-    <header className="w-full bg-main top-0 border-b-2">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <Link href="/" className="text-xl font-mono">
-              Jutjubić
-            </Link>
+    <>
+      <header className="w-full bg-main top-0 fixed border-b-2">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <Link href="/" className="text-xl font-mono">
+                Jutjubić
+              </Link>
+            </div>
+            <nav className="flex space-x-4">
+              <Suspense
+                fallback={
+                  <Skeleton className="rounded-full size-11 aspect-square" />
+                }
+              >
+                <HeaderAvatarButton />
+              </Suspense>
+            </nav>
           </div>
-          <nav className="flex space-x-4">
-            <Suspense
-              fallback={
-                <Skeleton className="rounded-full size-11 aspect-square" />
-              }
-            >
-              <HeaderAvatarButton />
-            </Suspense>
-          </nav>
         </div>
-      </div>
-    </header>
+      </header>
+      <div className="h-16" />
+    </>
   );
 }
