5ebe2ef47485ddd11493961a42c0ee21a82f0e05
Vladimir Zoric <vladimir.zorke@gmail.com>
2026-01-09 09:00:32 +0100

    feat-fix(frondent): add home pagination and fix comment pagination

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,10 +1,17 @@
 import HomeFeed from "@/components/HomeFeed";
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const { page } = await searchParams;
+  const currentPage = page ? parseInt(page) : 0;
+
   return (
     <div className="bg-grid min-h-[80vh] p-3 md:p-8">
       <div className="max-w-screen-2xl mx-auto">
-        <HomeFeed />
+        <HomeFeed page={currentPage} />
       </div>
     </div>
   );
