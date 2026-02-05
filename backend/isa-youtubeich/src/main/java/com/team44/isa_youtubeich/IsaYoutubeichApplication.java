904b0ca16761178f2d3e2f4daa8a594d361983d4
Mile Vuletić <vuletic.ra45.2022@uns.ac.rs>
2026-02-05 13:44:26 +0100

    chore: configure schedlock to use redis for synchronization

--- backend/isa-youtubeich/src/main/java/com/team44/isa_youtubeich/IsaYoutubeichApplication.java
+++ backend/isa-youtubeich/src/main/java/com/team44/isa_youtubeich/IsaYoutubeichApplication.java
@@ -1,5 +1,6 @@
 package com.team44.isa_youtubeich;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class IsaYoutubeichApplication {
 
 	public static void main(String[] args) {
