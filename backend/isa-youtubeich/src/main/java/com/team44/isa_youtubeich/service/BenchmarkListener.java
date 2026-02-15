39567ce66d81111d5baa75135ba2b2a2cf9e6c58
Igor Ludajic <igor.ludajic@gmail.com>
2026-02-15 21:08:36 +0100

    fix: add message send on upload

--- backend/isa-youtubeich/src/main/java/com/team44/isa_youtubeich/service/BenchmarkListener.java
+++ backend/isa-youtubeich/src/main/java/com/team44/isa_youtubeich/service/BenchmarkListener.java
@@ -1,12 +1,23 @@
 package com.team44.isa_youtubeich.service;
 
+import com.team44.isa_youtubeich.dto.UploadEventJsonDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.json.JsonMapper;
 
 @Service
 public class BenchmarkListener {
 
+    private final JsonMapper jsonMapper = JsonMapper.builder().build();
+
     @RabbitListener(queues = "benchmark_queue")
     public void receiveMessage(byte[] message) {
+        try {
+            UploadEventJsonDto event = jsonMapper.readValue(message, UploadEventJsonDto.class);
+            System.out.println("BenchmarkListener: primljena poruka za video: " + event.getVideoTitle());
+
+        } catch (Exception e) {
+            System.err.println("Greška pri čitanju poruke u Listeneru: " + e.getMessage());
+        }
     }
 }
